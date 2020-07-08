94b2345bbc1c4439b2692dcdc8004c241d1d1056
hanfenglei <18810285679@163.com>
2020-07-08 22:44:34 +0800

    tabbar未完成

---
+++ src/components/common/TabBarItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="tab-bar-item">
+    <slot slot="item-icon" />
+    <slot slot="item-text" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+};
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+
+.tab-bar-item img {
+  width: 24px;
+  height: 24;
+  margin-top: 3px;
+}
+</style>
