4baca81f68669bbf01fd185f3cb27868bf695de3
hanfenglei KF6425 (RD) <hkf6425@h3c.huawei-3com.com>
2020-07-09 13:56:02 +0800

    tabbar-ok

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,21 @@
         <img src="~assets/img/tabbar/home.svg" alt slot="item-icon" />
         <img src="~assets/img/tabbar/home_active.svg" alt slot="item-icon-active" />
         <div slot="item-text">首页</div>
+      </TabBarItem>
 
+      <TabBarItem>
         <img src="~assets/img/tabbar/category.svg" alt slot="item-icon" />
         <img src="~assets/img/tabbar/category_active.svg" alt slot="item-icon-active" />
         <div slot="item-text">分类</div>
+      </TabBarItem>
 
+      <TabBarItem>
         <img src="~assets/img/tabbar/cart.svg" alt slot="item-icon" />
         <img src="~assets/img/tabbar/cart_active.svg" alt slot="item-icon-active" />
         <div slot="item-text">购物车</div>
+      </TabBarItem>
 
+      <TabBarItem>
         <img src="~assets/img/tabbar/profile.svg" alt slot="item-icon" />
         <img src="~assets/img/tabbar/profile_active.svg" alt slot="item-icon-active" />
         <div slot="item-text">我的</div>
@@ -23,8 +29,8 @@
 </template>
 
 <script>
-const TabBar = import("components/common/tabbar/TabBar.vue");
-const TabBarItem = import("components/common/tabbar/TabBarItem.vue");
+const TabBar = () => import("components/common/tabbar/TabBar.vue");
+const TabBarItem = () => import("components/common/tabbar/TabBarItem.vue");
 
 export default {
   name: "app",
