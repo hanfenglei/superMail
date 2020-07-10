f4c32fe6b7aa233b188a9e6dcab1faadd3a982a1
hanfenglei KF6425 (RD) <hkf6425@h3c.huawei-3com.com>
2020-07-10 17:48:24 +0800

    bianma2

---
+++ src/views/cart/childComps/CartList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <scroll>
+      <div>
+        <caft-list-item v-for="item in cartList" :key="item.iid" :item-info="item"></caft-list-item>
+      </div>
+    </scroll>
+  </div>
+</template>
+
+
+<script>
+import Scroll from "common/scroll/Scroll";
+import CartListItem from "./CartListIetm";
+
+export default {
+  name: "CartList",
+
+  components: {
+    Scroll,
+    CartListItem
+  },
+
+  props: {
+    cartList: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+</style>
